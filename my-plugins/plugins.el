80bf8d68ba819d7f51e01d00350e1c16d346bb92
Orange_Karton <anpaschenko@gmail.com>
2026-01-12 22:51:44 +0200

    my cmake project config basic impelementation

--- my-plugins/plugins.el
+++ my-plugins/plugins.el
@@ -1,4 +1,7 @@
+;; -*- lexical-binding: t -*-
+
 (defun insert-define-guard()
+  "Insert a C-style header guard at the beginning and end of the buffer."
   (interactive)
   (let ((define-name (upcase (replace-regexp-in-string "\\." "_" (buffer-name)))))
     (message "Define name %s" define-name)
@@ -6,7 +9,7 @@
       (goto-char (point-min))
       (insert (format "#ifndef %s\n#define %s\n" define-name define-name))
       (goto-char (point-max))
-      (insert (format "#endif //%s" define-name)))))
+      (insert (format "\n#endif //%s" define-name)))))
 
 (defun copy-buffer-name-to-clipboard ()
   "Copy the current buffer name to the clipboard."
@@ -25,4 +28,55 @@
 
 
 
+;; TODO try to fix these wierd errors
+;; error in process sentinel: let: Symbol’s value as variable is void: project-root
+;; error in process sentinel: Symbol’s value as variable is void: project-root
 
+(defun my-cmake-config ()
+  "Run cmake in the project's build directory."
+  (interactive)
+  ;; Find the project root by locating CMakeLists.txt
+  (let* ((my-project-root (project-root (project-current)))
+         (cmake-lists (locate-dominating-file my-project-root "CMakeLists.txt")))
+    (unless my-project-root
+      (error "Setup project first"))
+    
+    (unless cmake-lists
+      (error "CMakeLists.txt in root"))
+
+    (global-set-key (kbd "<f5>") 'my-cmake-build)
+    ;; Run the build using Emacs' compile command
+    (let ((default-directory my-project-root))
+      (compile "cmake -S . -B build/Release -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake -S . -B build/Debug -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+      (add-hook 'compilation-finish-functions
+                (lambda (_buf _msg)
+                  (let ((src (expand-file-name "build/Debug/compile_commands.json" my-project-root))
+                        (dst (expand-file-name "compile_commands.json" my-project-root)))
+                    (when (file-exists-p src)
+                      (copy-file src dst t)
+                      (message "Copied %s -> %s" src dst))))))))
+
+(defun my-cmake-build ()
+  "Run cmake in the project's build directory."
+  (interactive)
+  ;; Find the project root by locating CMakeLists.txt
+  (let* ((project-root (project-root (project-current)))
+         (build-dir (locate-dominating-file project-root "CMakeLists.txt")))
+    (unless project-root
+      (error "Setup project first"))
+    
+    ;; Run the build using Emacs' compile command
+    (let ((default-directory project-root))
+      (compile "cmake --build build/Debug"))))
+
+(defun my-cmake-debug()
+  "Run cmake in the project's build directory."
+  (interactive)
+  ;; Find the project root by locating CMakeLists.txt
+  (let* ((project-root (project-root (project-current)))
+         (build-dir (expand-file-name "build" project-root)))
+    (unless project-root
+      (error "Setup project first"))
+    ;; Run the build using Emacs' compile command
+    (let ((default-directory project-root))
+      (gdb "build/Debug/src/HelloFriend"))))

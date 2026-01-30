2b9ec46a39b5163488f9132dae6efc2d79ce75ee
Karton <anpaschenko@gmail.com>
2026-01-30 21:30:25 +0200

    * my-plugins/plugins.el: added custom line bar

--- my-plugins/plugins.el
+++ my-plugins/plugins.el
@@ -1,24 +1,6 @@
 ;; -*- lexical-binding: t -*-
 
 
-(defvar-local my-cmake-lighter " Debug"
-  "Buffer-local lighter for `my-mode'.")
-
-(defvar-local my-cmake-config-var "Debug"
-  "Buffer-local lighter for `my-mode'.")
-
-(defun my-cmake-switch-config (config)
-  "Toggle the mode line lighter in the current buffer."
-  (interactive
-   (list (completing-read "Choose config: " '("Debug" "Release"))))
-  (setq my-cmake-lighter (format " %s" config))
-  (setq my-cmake-config-var config)
-  (force-mode-line-update))
-
-(define-minor-mode my-cmake
-  "A custom minor mode with buffer-local lighter."
-  :lighter my-cmake-lighter)
-
 (defun insert-define-guard()
   "Insert a C-style header guard at the beginning and end of the buffer."
   (interactive)
@@ -45,15 +27,59 @@
         (message "Copied file path: %s" buffer-file-name))
     (message "This buffer is not visiting a file.")))
 
+;; ---------------------------------------------------------------------------------------------
 
+(defvar my-cmake-generators '("Ninja Multi-Config" "Unix Makefiles"))
+
+(defvar my-cmake-session-configs nil)
+
+(defun my-cmake-get-project-config (project)
+  (let ((config (cdr (assoc project my-cmake-session-configs))))
+    (unless config
+      (push (cons project "Debug") my-cmake-session-configs)
+      (setq config "Debug"))
+    config))
+
+(defun my-cmake-is-cmake-project ()
+  (let* ((my-project-root (project-root (project-current)))
+         (cmake-lists (locate-dominating-file my-project-root "CMakeLists.txt")))
+    (and my-project-root cmake-lists)))
+
+(defun my-cmake-mode-line ()
+  "Return the current CMake config for mode line."
+  
+  (when (my-cmake-is-cmake-project)
+    (format " Config:%s" (my-cmake-get-project-config (project-root (project-current))))))
+
+;; Add to mode line globally
+(unless (member '(:eval (my-cmake-mode-line)) mode-line-format)
+  (setq-default mode-line-format
+                (append mode-line-format '((:eval (my-cmake-mode-line))))))
+
+(defun my-cmake-switch-config (config)
+  "Switch the CMake config for the current project."
+  (interactive
+   (list (completing-read "Choose config: " '("Debug" "Release"))))
+  (let ((config-pair (assoc (project-root (project-current))
+                            my-cmake-session-configs)))
+    (if config-pair
+        (setcdr config-pair config)
+      (push (cons (project-root (project-current)) config)
+            my-cmake-session-configs)))
+  ;; Update all mode lines
+  (force-mode-line-update t))
+
+(define-minor-mode my-cmake
+  "A custom minor mode with buffer-local lighter.")
 
 ;; TODO try to fix these wierd errors
 ;; error in process sentinel: let: Symbol’s value as variable is void: project-root
 ;; error in process sentinel: Symbol’s value as variable is void: project-root
 
-(defun my-cmake-config ()
+(defun my-cmake-config (generator)
   "Run cmake in the project's build directory."
-  (interactive)
+  (interactive
+   (list (completing-read "Generator: " my-cmake-generators)))
   ;; Find the project root by locating CMakeLists.txt
   (let* ((my-project-root (project-root (project-current)))
          (cmake-lists (locate-dominating-file my-project-root "CMakeLists.txt")))
@@ -66,7 +92,8 @@
     (global-set-key (kbd "<f5>") 'my-cmake-build)
     ;; Run the build using Emacs' compile command
     (let ((default-directory my-project-root))
-      (compile "cmake -S . -B build/Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON && cmake -S . -B build/Debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON")
+      ;; (compile (format "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G \"%s\"" generator))
+      (compile (format "cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON" generator))
       (add-hook 'compilation-finish-functions
                 (lambda (_buf _msg)
                   (let ((src (expand-file-name "build/Debug/compile_commands.json" my-project-root))
@@ -86,7 +113,7 @@
     
     ;; Run the build using Emacs' compile command
     (let ((default-directory project-root))
-      (compile (format "cmake --build build/%s --config %s" my-cmake-config-var my-cmake-config-var)))))
+      (compile (format "cmake --build build --config %s" (my-cmake-get-project-config (project-root (project-current))))))))
 
 (defun my-cmake-debug()
   "Run cmake in the project's build directory."
